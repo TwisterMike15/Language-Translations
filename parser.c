938cd8fd8e3e746a0eecb44c9407453d67e50b9e
langtrang2 <61919656+langtrang2@users.noreply.github.com>
2020-03-07 16:30:14 -0500

    parser.c
    added to the production prototypes and the production operations

--- parser.c
+++ parser.c
@@ -18,6 +18,20 @@ void systemgoal();
 void program();
 void statementlist();
 void statement();
+void IFTail();
+void idList();
+void exprList();
+void expression();
+void term();
+void factor();
+void addop();		//do we need this here?
+void multop();		//do we need this here?
+void condition();
+void addition();
+void multiplication();
+void unary();
+void lprimary();
+void RelOp();
 
 
 //Productions
@@ -38,27 +52,132 @@ void statementlist() {
 
 void statement() {
 	switch (CurrToken.Id) {
-		case ID:
-			match(ID);
-			match(ASSIGNOP);
-			//expression();
+	case ID:
+		match(ID);
+		match(ASSIGNOP);
+		expression();
 
-			break;
-		case READ:
-			match(READ);
-			match(LPAREN);
-			//idlist();
+		break;
+	case READ:
+		match(READ);
+		match(LPAREN);
+		idList();
+		match(RPAREN);
 
-			break;
-		case WRITE:
-			//Blah
+		break;
+	case WRITE:
+		match(WRITE);
+		match(LPAREN);
+		exprList();
+		match(RPAREN);
 
-			break;
-		case IF:
+		break;
+	case IF:
+		match(IF);
+		match(LPAREN);
+		condition();
+		match(THEN);
+		statementlist();
+		IFTail();
 
-			break;
-		default:
+		break;
+	case WHILE:
+		match(WHILE);
+		match(LPAREN);
+		condition();
+		match(RPAREN);
+		statementlist();
+		match(ENDWHILE);
 
-			break;
+		break;
+	default:
+
+		break;
 	}
 }
+
+void IFTail() {
+	switch (CurrToken.Id) {
+	case ELSE:
+		match(ELSE);
+		statementlist();
+		match(ENDIF);
+
+		break;
+	case ENDIF:
+		match(ENDIF);
+
+		break;
+	default:
+
+		break;
+	}
+}
+
+void idList() {
+		match(ID);
+		match(COMMA);
+		match(ID);
+}
+
+void exprList() {
+	expression();
+	match(COMMA);
+	//expression();
+}
+
+void expression() {
+	term();
+	addop();
+	//term();
+}
+
+void term() {
+	factor();
+	multop();
+	//factor();
+}
+
+//	THIS NEEDS FURTHER WORK
+void factor() {
+	switch (CurrToken.Id) {
+	case LPAREN:		//'('
+		match(LPAREN);
+		expression();
+		match(RPAREN);
+
+		break;
+	case '-':
+
+		break;
+	default:
+
+		break;
+	}
+}
+
+//	PRODUCE THE ADDOP AND MULTOP HERE
+
+void condition() {
+	addition();
+	RelOp();
+	//addition();
+}
+
+void addition() {
+	multiplication();
+	addop();
+	//multiplication();
+}
+
+void multiplication() {
+	unary();
+	multop();
+	//unary();
+}
+
+// void unary()
+
+// void lprimary()
+
+// void RelOp()
