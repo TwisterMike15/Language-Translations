625b7f9cb0ae74eeb7ff2b7f1bea044057783ef3
langtrang2 <61919656+langtrang2@users.noreply.github.com>
2020-03-15 17:41:43 -0400

    Update file_util.h
    Only re added my name

--- file_util.h
+++ file_util.h
@@ -3,6 +3,7 @@ Paul Maclean- mac7537@calu.edu
 Mike Gorse- gor9632@calu.edu
 Robert Breckenridge- bre6896@calu.edu
 Chase Smith- smi8808@calu.edu
+Rudolph Hanzes - han7739@calu.edu
 
 CSC 460
 Language Translations
